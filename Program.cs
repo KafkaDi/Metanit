653826fc34f62f4d17a4ce45a709622aed1dd323
Dmitriy Timonin <timonin.ask@gmail.com>
2019-08-19 15:02:26 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User tom = new User(); //пользуемся к-ром по умолч
+            tom.name = "Tom";
+            tom.age = 22;
+            tom.Info();
+
+            User bob = new User("Bob"); //пользуемся 1м к-ром
+            bob.name = "Bob";
+            bob.age = 33;
+            bob.Info();
+
+            User kot = new User("Pupuska", 1); //польз-ся 2м к-ром
+            kot.Info();
+
+            User pok = new User { age = 146, name = "procent" }; //передаём переметры во 2й конструктор через инициализатор (напрямую) 
+            pok.Info();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class User
+    {
+        public string name; // поля класса
+        public int age;
+
+        public User(string name)  //пользовательский к-тор в () параметр конструктора а зис. - это поле класса
+        {
+            this.name = name;
+        }
+        public User()  //пустой конструктор, так же создаётся по умолчанию когда нет других к-ров
+        {
+
+        }
+        public User(string n, int age) : this(n)  //пользовательский к-тор в нём 1й парам. заимствуется у верхнего к-ра (n)
+        {
+            this.age = age;
+        }
+        public void Info() //поведение класса
+        {
+            Console.WriteLine($"{name} - {age}");
+        }
+    }
+}
